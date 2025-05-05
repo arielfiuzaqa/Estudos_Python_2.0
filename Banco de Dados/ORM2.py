65659fb546cbe39da03cbf30ee6878f8f7f90eed
arielfiuzaqa <arielfiuzaqa@gamil.com>
2025-05-04 21:22:18 -0300

    Revisão: 4 - Select

--- Banco de Dados/ORM2.py
+++ Banco de Dados/ORM2.py
@@ -12,16 +12,26 @@ def RetornaSession():
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
-session = RetornaSession()
 
+session = RetornaSession()
+x1 = session.query(Pessoas).all() # Retorna todos os objetos da tabela Pessoas
+x2 = session.query(Pessoas).filter(Pessoas.nome=='Lucas') # Retorna o primeiro objeto que atende ao filtro
+#print(x1[0].id) # Imprime o id do primeiro objeto da lista x1
+x3 = session.query(Pessoas).filter_by(nome='Fabio', usuario='Cofabi123')
+
+for i in x3:
+    print(i.id) # Imprime o nome de cada objeto da lista x1
+
+'''
 x = Pessoas(nome='Lucas', 
             usuario='Lukinha69', 
             senha=171869)
 y = Pessoas(nome='Fabio', 
             usuario='Cofabi123', 
-            senha=181899)
+            senha=181899)'''
 
 #session.add(x) # Adiciona o objeto à sessão
-session.add_all([x, y]) # Adiciona todos os objetos à sessão
-session.rollback() # Desfaz as alterações na sessão
-session.commit() # Salva as alterações no banco de dados
+#session.add_all([x, y]) # Adiciona todos os objetos à sessão
+#session.rollback() # Desfaz as alterações na sessão
+#session.commit() # Salva as alterações no banco de dados
+
