cdd081eab240e326c8f03f263ddfb6fef668dc36
arielfiuzaqa <arielfiuzaqa@gamil.com>
2025-05-07 08:12:30 -0300

    Revisão: 7 Deletar objetos

--- Banco de Dados/ORM2.py
+++ Banco de Dados/ORM2.py
@@ -15,21 +15,32 @@ def RetornaSession():
     return session
 
 session = RetornaSession()
-x1 = session.query(Pessoas).all() # Retorna todos os objetos da tabela Pessoas
+# Deletando dados
+'''z = session.query(Pessoas).filter(Pessoas.id == 3).delete() # Deleta o objeto que atende ao filtro
+session.commit() # Salva as alterações no banco de dados'''
+# Deletando dados com filtro one()
+z = session.query(Pessoas).filter(Pessoas.id == 2).one() # Deleta o objeto que atende ao filtro
+session.delete(z) # Deleta o objeto da sessão
+session.commit() # Salva as alterações no banco de dados
+
+'''x1 = session.query(Pessoas).all() # Retorna todos os objetos da tabela Pessoas
 x2 = session.query(Pessoas).filter(Pessoas.nome=='Lucas') # Retorna o primeiro objeto que atende ao filtro
 #print(x1[0].id) # Imprime o id do primeiro objeto da lista x1
 x3 = session.query(Pessoas).filter_by(nome='Fabio', usuario='Cofabi123')
-
+'''
 '''for i in x3:
     print(i.id) # Imprime o nome de cada objeto da lista x1
 '''
+'''
+# Filtrando e alterando dados
 y = session.query(Pessoas).filter(Pessoas.id == 5).all() # Retorna todos os objetos que atendem a pelo menos um dos filtros
 y[0].nome = 'Joana' # Altera o nome do primeiro objeto da lista y
 y[0].senha = 'novaSenha' # Altera a senha do primeiro objeto da lista y
 y[0].usuario = 'jjkf02' # Altera o nome do primeiro objeto da lista y
 session.commit() # Salva as alterações no banco de dados
-
 '''
+'''
+# Adicionando dados
 x = Pessoas(nome='Lucas', 
             usuario='Lukinha69', 
             senha=171869)
